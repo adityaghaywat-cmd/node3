ee566b1366dba9223e1940dec84e63e22c9e05aa
adityaghaywat-cmd <adityaghaywat34@gmail.com>
2026-06-08 22:35:29 +0530

    Change greeting from 'Hello Aditya' to 'Hello world'

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
                 <title>Jenkins Node App</title>
             </head>
             <body>
-                <h1>Hello Aditya...</h1>
+                <h1>Hello world...</h1>
             </body>
         </html>
     `);
